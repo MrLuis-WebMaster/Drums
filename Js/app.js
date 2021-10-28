1dbe1b7d837d8b0c4ff9172e2dddc397e692da51
Luis <MrLuis-WebMaster>
2021-10-27 21:15:37 -0500

    version beta 1.0

--- Js/app.js
+++ Js/app.js
@@ -3,11 +3,10 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 function IniciarApp() {
-    window.addEventListener("click",AudioTecla)
-    window.addEventListener("click",AnimacionTecla)
+    window.addEventListener("touchstart",AudioTecla2)
+    window.addEventListener("touchstart",AnimacionTecla2)
     window.addEventListener("keydown", AudioTecla);
     window.addEventListener("keydown", AnimacionTecla)
-
 }
 
 function AudioTecla(e) {
@@ -16,7 +15,21 @@ function AudioTecla(e) {
     audio.currentTime = 0;
     audio.play();
 }
+function AudioTecla2(e) {
+    const audio2 = document.querySelector(`audio[data-key="${e.path[1].dataset.key}"]`);
+    if(!audio2) return;
+    audio2.currentTime = 0;
+    audio2.play();
+}
+function AnimacionTecla2(e) {
+    const key = document.querySelector(`.key[data-key="${e.path[1].dataset.key}"]`);
+    if(!key) return;
+    key.classList.add("playing");
 
+    const keys = document.querySelectorAll(".key");
+    keys.forEach( key => key.addEventListener("transitionend",RemoveTransition))
+    
+}
 function AnimacionTecla(e) {
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!key) return;
